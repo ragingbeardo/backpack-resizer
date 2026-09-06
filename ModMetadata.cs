2b1b789120f98a864ee97ab20b44d538b407979a
RagingBeardo <beardos.services@gmail.com>
2026-09-06 11:27:45 -0400

    bump spt version range to ~4.1.3

--- ModMetadata.cs
+++ ModMetadata.cs
@@ -18,7 +18,7 @@ public record ModMetadata : IModMetadata, IModBlazorMetadata
     public List<string>? Contributors { get; init; }
     public SemanticVersioning.Version Version { get; init; } = new("1.0.1");
     
-    public SemanticVersioning.Range SptVersion { get; init; } = new("~4.1.0");
+    public SemanticVersioning.Range SptVersion { get; init; } = new("~4.1.3");
     public List<string>? Incompatibilities { get; init; }
     public Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
     
